20ec4c7ef998837cc06589a246748a3b33494e25
Pablo López Torres <pablolopeztorres@gmail.com>
2011-07-15 17:45:56 +0200

    standarizing emails

--- common/init.d/bashmarks.sh
+++ common/init.d/bashmarks.sh
@@ -1,5 +1,5 @@
 # Copyright (c) 2010, Huy Nguyen, http://www.huyng.com
-# Modified by Pablo Lopez Torres <pablo@redcore.es>
+# Modified by Pablo Lopez Torres <pablolopeztorres@gmail.com>
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided 
